2fbe863643da3a4f09dd418d3f01cd7d587594d7
Devasaya <devasaya@stacx.io>
2024-10-22 23:07:10 +0530

    Final testing is remaining.

--- my_try.cpp
+++ my_try.cpp
@@ -8,6 +8,7 @@ using namespace cv;
 int main() {
     vector<Scalar> colors = {Scalar(255, 0, 0), Scalar(255, 0, 255), Scalar(0, 255, 0), Scalar(0, 0, 255), Scalar(0, 255, 255)};
     Scalar color = colors[0];
+    int min_area = 1000;
 
     VideoCapture cam(0);
     if(!cam.isOpened()){
@@ -17,7 +18,7 @@ int main() {
 
     int width = static_cast<int>(cam.get(CAP_PROP_FRAME_WIDTH));
     int height = static_cast<int>(cam.get(CAP_PROP_FRAME_HEIGHT));
-    Mat mat = Mat::zeros(height,width, CV_8UC3);
+    Mat canvas = Mat::zeros(height,width, CV_8UC3);
 
     // handles light red
     Scalar lowerRed1(0, 100, 100);
@@ -28,6 +29,8 @@ int main() {
     Scalar upperRed2(180, 255, 255);
 
     Mat kernel = getStructuringElement(MORPH_RECT, Size(10, 10));
+
+    Point prevCenter = Point(0, 0);
 
     // render processing loop
     while (true) {
@@ -49,8 +52,6 @@ int main() {
         bitwise_or(mask1, mask2, mask);
         dilate(mask, mask, kernel);
 
-        imshow("Mask: ",mask);
-
         vector<vector<Point>> contours;
         findContours(mask, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
@@ -63,29 +64,65 @@ int main() {
                 }
             );
 
-            // center of the contour
-            Moments M = moments(largestContour);
-            int cX = int(M.m10 / M.m00);
-            int cY = int(M.m01 / M.m00);
+            double area = contourArea(largestContour);
 
-            circle(frame, Point(cX, cY), 10, Scalar(0, 0, 255), 2);
+            if (area > min_area) {
+                Moments M = moments(largestContour);
+                int cX = int(M.m10 / M.m00);
+                int cY = int(M.m01 / M.m00);
 
-            if (cY < 65 && (20 < cX && cX < 120)) {
-                Mat canvas = Mat::zeros(height, width, CV_8UC3);
-            } else if (cY < 65) {
-                if (140 < cX && cX < 220) color = colors[0];
-                else if (240 < cX && cX < 320) color = colors[1];
-                else if (340 < cX && cX < 420) color = colors[2];
-                else if (440 < cX && cX < 520) color = colors[3];
-                else if (540 < cX && cX < 620) color = colors[4];
+                circle(frame, Point(cX, cY), 10, Scalar(0, 0, 255), 2);
+
+                if (cY < 65 && (20 < cX && cX < 120)) {
+                    canvas = Mat::zeros(height, width, CV_8UC3);
+                } else if (cY < 65) {
+                    if (140 < cX && cX < 220) color = colors[0];
+                    else if (240 < cX && cX < 320) color = colors[1];
+                    else if (340 < cX && cX < 420) color = colors[2];
+                    else if (440 < cX && cX < 520) color = colors[3];
+                    else if (540 < cX && cX < 620) color = colors[4];
+                }
+
+                if(prevCenter != Point(0, 0)) {
+                    line(canvas, prevCenter, Point(cX, cY), color, 2);
+                }
+                prevCenter = Point(cX, cY);
+            } else {
+                prevCenter = Point(0, 0);
             }
-            Point prev_center = ;
-            if ()
         }
 
+        Mat canvas_gray, canvas_binary;
+        cvtColor(canvas, canvas_gray, COLOR_BGR2GRAY);
+        threshold(canvas_gray, canvas_binary, 20, 255, THRESH_BINARY_INV);
+        cvtColor(canvas_binary, canvas_binary, COLOR_GRAY2BGR);
+
+        bitwise_and(frame, canvas_binary, frame);
+        bitwise_or(frame, canvas, frame);
+
+        rectangle(frame, Point(20, 1), Point(120, 65), Scalar(122, 122, 122), -1);
+        rectangle(frame, Point(140, 1), Point(220, 65), colors[0], -1);
+        rectangle(frame, Point(240, 1), Point(320, 65), colors[1], -1);
+        rectangle(frame, Point(340, 1), Point(420, 65), colors[2], -1);
+        rectangle(frame, Point(440, 1), Point(520, 65), colors[3], -1);
+        rectangle(frame, Point(540, 1), Point(620, 65), colors[4], -1);
+
+        putText(frame, "CLEAR ALL", Point(30, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+        putText(frame, "BLUE", Point(155, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+        putText(frame, "VIOLET", Point(255, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+        putText(frame, "GREEN", Point(355, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+        putText(frame, "RED", Point(465, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+        putText(frame, "YELLOW", Point(555, 33), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255, 255, 255), 2, LINE_AA);
+
+        imshow("Frame", frame);
+        imshow("Canvas", canvas);
+
+        if (waitKey(1) && 0xFF == 'q') {
+            break;
+        }
     }
 
-
-
+    cam.release();
+    destroyAllWindows();
     return 0;
 }
